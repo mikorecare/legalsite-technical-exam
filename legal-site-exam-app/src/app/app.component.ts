000f5a5d5301b6b0dc2be6432d63305393ab0c24
SvenSven <mikorecare@gmail.com>
2025-02-21 06:26:41 +0800

    FEAT: added form text area component

--- legal-site-exam-app/src/app/app.component.ts
+++ legal-site-exam-app/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadNavigationListComponent } from './components/head-navigation-list/head-navigation-list.component';
 import { SpeechListComponent } from "./components/speech-list/speech-list.component";
+import { FormTextAreaComponent } from "./components/form-text-area/form-text-area.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     HeadNavigationListComponent,
-    SpeechListComponent
+    SpeechListComponent,
+    FormTextAreaComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
---
+++ legal-site-exam-app/src/app/components/form-text-area/form-text-area.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "form-text-area-component",
+    templateUrl: "./form-text-area.component.html",
+    standalone: true
+})
+export class FormTextAreaComponent {
+    constructor(){}
+}
