69e0608b2b4b61bdfb9858a63eda2b7c939d96ab
SvenSven <mikorecare@gmail.com>
2025-02-21 04:39:17 +0800

    FEAT: Added HeadList Component

--- legal-site-exam-app/src/app/app.component.ts
+++ legal-site-exam-app/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadNavigationListComponent } from './components/head-navigation-list/head-navigation-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeadNavigationListComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ legal-site-exam-app/src/app/components/head-navigation-list/head-navigation-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "head-navigation-list-component",
+    templateUrl: "./head-navigation-list.component.html",
+    standalone: true
+})
+export class HeadNavigationListComponent {
+    constructor(){}
+}
